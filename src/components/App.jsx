fd4c37114f8981ae8438fcb148c82ea480cb7736
Maryna Medynska <maryna.medynska@gmail.com>
2023-03-06 23:54:30 +0200

    Finished hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+import { Toaster } from 'react-hot-toast';
+
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+
+class App extends Component {
+
+  state = {
+    inputName: '',
+  }
+
+  // https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12
+// Pixabay API підтримує пагінацію, за замовчуванням параметр page дорівнює 1. Нехай у відповіді надходить по 12 об'єктів, встановлено в параметрі per_page. Не забудь, що під час пошуку за новим ключовим словом, необхідно скидати значення page до 1.
+  
+  haldleFormSubmit = inputName => {
+    this.setState({inputName})
+  }
+    
+  render() {
+    const { hits, inputName } = this.state;
+    return (
+      <div>
+        <Searchbar onSubmit={this.haldleFormSubmit} />
+        <ImageGallery options={hits} inputName={inputName}   />
+        <Toaster />
+      </div>
+    );
+  }
 };
+
+export default App;
---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,13 @@
+// import PropTypes from 'prop-types';
+
+// const SearchForm = (onSubmit) => {
+//     return ( 
+
+//     )
+// }
+
+// export default SearchForm; 
+
+// SearchForm.propTypes = {
+//     onSubmit: PropTypes.func.isRequired,
+// }
